94b6a25681a46b52bbcc4b628ad08f0ca81e1a0d
alexander <naitwa.alexandre@gmail.com>
2025-02-27 16:30:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-500">
+      <h1 className="text-3xl font-bold text-white">Tailwind is Working! 🚀</h1>
+    </div>
+  );
+};
+
+export default App;
